5cf8f19ba6e430dadcca4aea3340e9ae771634e4
Scott <scott@kloudstack.me>
2024-06-16 00:08:04 +0100

    Commit setup.sh

---
+++ setup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -e
+
+# -------------------------------------------
+# Ensure we are running the updater  as root
+# -------------------------------------------
+if [[ $EUID -ne 0 ]]; then
+  echo "  Aborting because you are not root" ; exit 1
+fi
+
+cat <<EOF
+  -----------------------------------------------------------------
+  This script sets the srv_hostname and updates it fully.
+  -----------------------------------------------------------------
+EOF
+
+# -------------------------------------------
+# Vars
+# -------------------------------------------
+SMTP_TO=""
+SMTP_FROM=""
+SMTP_SERVER=""
+SMTP_USER=""
+SMTP_AUTH=''
+
+########################
+# functions start here #
+########################
+
+function start_script {
+echo "Please type your full hostname"
+echo "Example: subdomain.domain"
+echo ""
+read NHST
+# Set names
+sudo hostnamectl set-hostname ${NHST}
+sudo hostnamectl set-hostname ${NHST} --pretty
+echo "Your hostname is now ${NHST}"
+sleep 1
+clear
+echo "Updating ${NHST}"
+sudo apt-get -qq install -y swaks 2> /dev/null
+}
+
+function end_script {
+./update-minor.sh
+}
+
+###### Script start
+echo "Let's get started..."
+echo ""
+if start_script
+then
+ end_script
+ exit 0
+else
+ echo " Setup failed - sending email"
+ swaks --to ${SMTP_TO} --from ${SMTP_FROM} --server ${SMTP_SERVER} --auth-user ${SMTP_USER} --auth-password ${SMTP_AUTH} --body "failed to update on $(date '+%Y-%m-%d')" --header 'Subject: UPDATE FAILED for ${HOSTNAME}' ; exit 1
+fi
